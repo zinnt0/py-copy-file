2bd97dd7674e9d5bb773ac7710d9ca9c717942a7
Tristan Zinn <tristanzinn@MacBook-Pro-von-Tristan.local>
2025-04-14 22:07:40 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,20 @@
 # write your code here
+def copy_file(command: str) -> None:
+    try:
+        cp = command.split(" ")[0]
+        if cp != "cp":
+            return
+        src = command.split(" ")[1]
+        dest = command.split(" ")[2]
+    except IndexError:
+        pass
+    else:
+        if src == dest:
+            return
+        try:
+            with open(src, "r")as src_file:
+                content = src_file.read()
+                with open(dest, "a") as dest_file:
+                    dest_file.write(content)
+        except FileNotFoundError:
+            pass
